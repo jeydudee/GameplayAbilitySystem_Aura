5c7536d98eb7961c52b1281a5a54a724857ab31e
Jacob Nguyen <nguyenjacob70@gmail.com>
2024-04-19 02:22:46 -0700

    Character and Enemy Blueprints

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -3,14 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Character/AuraCharacter.h"
+#include "Character/AuraCharacterBase.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacter
+class AURA_API AAuraEnemy : public AAuraCharacterBase
 {
 	GENERATED_BODY()
 	
