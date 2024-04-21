822788931508b526f6a65830a681ccb22b84a531
Jacob Nguyen <nguyenjacob70@gmail.com>
2024-04-21 16:11:20 -0700

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright 2024 - Jacob Nguyen
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
